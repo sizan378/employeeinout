65cbaab91a9324065fe5d70d378e39f426a6dd9c
Sizan Mahmud <59968225+sizan378@users.noreply.github.com>
2021-10-29 22:19:20 +0600

    Update settings.py

--- InOutTime/settings.py
+++ InOutTime/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sdl@h$gvzi^x8ah+2+eh)er+sua96o-h(5i71zfa8_=y%+0%4i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
