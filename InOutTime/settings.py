1a459f53ee808e9eca49283f22d34e1b761270fc
Sizan Mahmud <59968225+sizan378@users.noreply.github.com>
2021-10-29 22:22:12 +0600

    Update settings.py

--- InOutTime/settings.py
+++ InOutTime/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-sdl@h$gvzi^x8ah+2+eh)er+sua96o-h(5i71zfa8_=y%+0%4i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
